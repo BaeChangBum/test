62ed3f74d60330505aae592497e6eab7e966cb41
changbum <changbum10001@gmail.com>
2016-01-24 00:17:54 +0900

    dfdf

---
+++ 9-1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    int i;
+    int arr[12];
+
+    srand((unsigned) time(0));
+
+    for(i=0; i<12; i++)
+        arr[i] = rand() % 20;
+
+    printf("Ã·ÀÚ : ");
+    for(i=0; i<12; i++)
+        printf("%2d ",i);
+    printf("\n");
+
+    printf("¿ä¼Ò : ");
+    for(i=0; i<12; i++)
+        printf("%2d ",arr[i]);
+
+    return 0;
+}
